cc899dc0e716277a137da7197194686d3dec9214
LopezALexis10 <168349529+LopezALexis10@users.noreply.github.com>
2026-07-21 03:15:08 -0500

    Actualizar configuración de Amplify y variables de entorno

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,7 @@
 export const awsConfig = {
-  region: "us-east-1",
-  userPoolId: "us-east-1_LvhenGkVL",
-  userPoolClientId: "69sernaht6guhjpg30cbvhnmn",
-  httpApiUrl: "https://5fi4xlq4h8.execute-api.us-east-1.amazonaws.com",
-  webSocketUrl: "wss://9lcc8pllpg.execute-api.us-east-1.amazonaws.com/prod",
+  region: import.meta.env.VITE_AWS_REGION || "us-east-1",
+  userPoolId: import.meta.env.VITE_COGNITO_USER_POOL_ID || "us-east-1_LvhenGkVL",
+  userPoolClientId: import.meta.env.VITE_COGNITO_CLIENT_ID || "69sernaht6guhjpg30cbvhnmn",
+  httpApiUrl: import.meta.env.VITE_HTTP_API_URL || "https://5fi4xlq4h8.execute-api.us-east-1.amazonaws.com",
+  webSocketUrl: import.meta.env.VITE_WEBSOCKET_URL || "wss://9lcc8pllpg.execute-api.us-east-1.amazonaws.com/prod",
 };
